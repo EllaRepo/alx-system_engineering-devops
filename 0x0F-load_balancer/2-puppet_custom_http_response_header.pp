42e481d423995ea6dc0b3b5d04f8f6364e30e101
EllaRepo <eliaberango@gmail.com>
2023-09-04 15:47:13 -0400

    Add 2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+# Installs and configures HAproxy on lb-01 server
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
