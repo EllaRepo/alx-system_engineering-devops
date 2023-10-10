636276f0205ed79bfb5667b510253e9855918f3a
Eliab E <eliaberango@gmail.com>
2023-10-10 12:52:03 -0700

    Removed inner for loop

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,6 +5,15 @@
 import requests
 
 
+def add_title(hot_list, posts):
+    """ Adds item into a list """
+    if len(posts) == 0:
+        return
+    hot_list.append(posts[0]['data']['title'])
+    posts.pop(0)
+    add_title(hot_list, posts)
+
+
 def recurse(subreddit, hot_list=[], after=None):
     """Queries the Reddit API and returns a list containing the titles of all
        hot articles for a given subreddit
@@ -18,9 +27,7 @@ def recurse(subreddit, hot_list=[], after=None):
                             allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        posts = data['data']['children']
-        if len(posts):
-            hot_list.append(posts[0]['data']['title'])
+        add_title(hot_list, data['data']['children'])
         if not data['data']['after']:
             return hot_list
         else:
