5f706c6165b334dc7d9a350f0e3a88277d6fd402
Eliab E <eliaberango@gmail.com>
2023-10-10 02:13:56 -0700

    Add 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Defines a function that queries the Reddit API and returns the
+    number of subscribers (not active users, total subscribers) for
+    a given subreddit. 
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
